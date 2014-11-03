3fd8b61741328965d04de7e9ecfffef26f7ab33e
James Hobin <hobinjk@mit.edu>
2014-11-03 14:52:17 -0500

    Add conversation property to PlayerData

--- public/javascripts/PlayerData.js
+++ public/javascripts/PlayerData.js
@@ -1,5 +1,13 @@
+/* jshint browser: true */
+/* exported PlayerData */
+
+/**
+ * A PlayerData object, used to coordinate information between states.
+ * @constructor
+ */
 function PlayerData() {
   this.location = null;
   this.completedGames = [];
   this.inventory = [];
+  this.conversation = null;
 }
