f8aa3dee8f7b5a78be119a1d16c79d177b325817
James Hobin <hobinjk@mit.edu>
2014-11-03 14:42:49 -0500

    Add scaffolds of Village and Conversation states

---
+++ public/javascripts/ConversationState.js
@@ -0,0 +1,25 @@
+/**
+ * @constructor
+ * Create a new ConversationState
+ */
+function ConversationState() {
+
+}
+
+/**
+ * Pre-load any assets required by the conversation
+ */
+ConversationState.prototype.preload = function() {
+};
+
+/**
+ * Create sprites and other game objects
+ */
+ConversationState.prototype.create = function() {
+};
+
+/**
+ * Update the Conversation
+ */
+ConversationState.prototype.update = function() {
+};
