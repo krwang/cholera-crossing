3ae2787b480f0dec6924c7e0b2a273a596378b8c
Rachel Wang <rswang@mit.edu>
2014-11-05 11:23:38 -0500

    Add openshift

--- app.js
+++ app.js
@@ -54,8 +54,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-var server = app.listen(app.get('port'));
+var server = app.listen(app.get('port'), app.get('ipaddress'));
 
 module.exports = app;
